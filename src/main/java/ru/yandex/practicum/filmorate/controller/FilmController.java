622b14fbe8e3fc2bd9fd1a0bb1e1a2e3a645705b
Georgy <vgera312@yandex.ru>
2022-09-18 00:12:07 +0400

    Filmorate 1.0

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -0,0 +1,68 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.validation.ValidationException;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+@Slf4j
+public class FilmController {
+
+    private int id;
+    private static final HashMap<Integer, Film> filmList = new HashMap<>();
+
+    @PostMapping("/films")
+    public void addFilm(@Valid @RequestBody Film film) {
+        if (validateFilm(film)) {
+            if (film.getId() == 0) {
+                film.setId(++id);
+            }
+            log.info("Идет процесс добавление нового фильма: {}", film.getName());
+            filmList.put(film.getId(), film);
+        }
+    }
+
+    @PutMapping("/films")
+    public void updateFilm(@Valid @RequestBody Film film) {
+        if (filmList.containsKey(film.getId())) {
+            log.info("Обновление фильма: {}", film.getName());
+            filmList.put(film.getId(), film);
+        } else {
+            throw new ValidationException("Фильм не найдем");
+        }
+    }
+
+    @GetMapping("/films")
+    public List<Film> allFilms() {
+        log.info("Идет процесс получения списка фильмов");
+        List<Film> films = new ArrayList<>(filmList.values());
+        return films;
+    }
+
+    private boolean validateFilm(Film film) {
+        if (!film.getName().isBlank()) {
+            if (film.getDescription().length() < 201) {
+                if (film.getReleaseDate().isAfter(LocalDate.of(1895, 12, 28))) {
+                    if (film.getDuration() > 0) {
+                        return true;
+                    } else {
+                        throw new ValidationException("Не правильно указана продолжительность!");
+                    }
+                } else {
+                    throw new ValidationException("Фильм слишком старый!");
+                }
+            } else {
+                throw new ValidationException("Длинное описание!");
+            }
+        } else {
+            throw new ValidationException("Неверное наименование!");
+        }
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,68 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.validation.ValidationException;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+@Slf4j
+public class UserController {
+
+    private int id;
+
+    private static final HashMap<Integer, User> userList = new HashMap<>();
+
+    @PostMapping("/users")
+    public void addUser(@Valid @RequestBody User user) {
+        if (validateUser(user)) {
+            if (user.getName() == null || user.getName().isBlank()) {
+                user.setName(user.getLogin());
+            }
+            if (user.getId() == 0) {
+                user.setId(++id);
+            }
+            log.info("Добавление нового пользователя: {}", user.getName());
+            userList.put(user.getId(), user);
+        }
+    }
+
+    @PutMapping("/users")
+    public void updateUser(@Valid @RequestBody User user) {
+        if (userList.containsKey(user.getId())) {
+            log.info("Обновление пользователя: {}", user.getLogin());
+            userList.put(user.getId(), user);
+        } else {
+            throw new ValidationException("Такого пользователя не существует");
+        }
+    }
+
+    @GetMapping("/users")
+    public List<User> allUsers() {
+        log.info("Запрос на получение списка пользователей");
+        List<User> users = new ArrayList<>(userList.values());
+        return users;
+    }
+
+    private boolean validateUser(User user) {
+        if (user.getEmail().contains("@") && !user.getEmail().isBlank()) {
+            if (!user.getLogin().isBlank() && !user.getLogin().contains(" ")) {
+                if (!user.getBirthday().isAfter(LocalDate.now())) {
+                    return true;
+                } else {
+                    throw new ValidationException("Дата рождения не прошла валидацию!");
+                }
+            } else {
+                throw new ValidationException("Логин не прошел валидацию!");
+            }
+        } else {
+            throw new ValidationException("Адрес эл.почты не прошел валидацию!");
+        }
+    }
+}
