414bfcd80b61f21044daf0311aaf00e43e138b9a
Georgy <vgera312@yandex.ru>
2023-01-23 02:10:14 +0400

    Filmorate 2.2

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,12 +1,15 @@
 package ru.yandex.practicum.filmorate;
 
 import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.event.annotation.BeforeTestExecution;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.FilmService;
@@ -27,21 +30,6 @@ class FilmorateApplicationTests {
     private final FilmService filmService;
     private final FriendService friendService;
     private final GenreService genreService;
-
-    @BeforeEach
-    public void beforeEachUsers() {
-        User user = createUser(1, "mail@yandex.ru", "login12", "mail", LocalDate.of(2001, 12, 1));
-        User user1 = createUser(2, "igor@yandex.ru", "igor123", "igor", LocalDate.of(2010, 11, 5));
-        User user2 = createUser(3, "misha@yandex.ru", "misha33", "misha", LocalDate.of(2008, 10, 4));
-        User user3 = createUser(4, "masha@yandex.ru", "masha33", "masha", LocalDate.of(2007, 9, 6));
-        User user4 = createUser(5, "ira@yandex.ru", "ira23", "ira", LocalDate.of(2006, 7, 7));
-        userService.addUser(user);
-        userService.addUser(user1);
-        userService.addUser(user2);
-        userService.addUser(user3);
-        userService.addUser(user4);
-
-    }
 
     @BeforeEach
     public void beforeEachFilms() {
@@ -68,31 +56,57 @@ class FilmorateApplicationTests {
     }
 
     @Test
-    public void testFindUser() {
-        Assertions.assertEquals("login12", userService.getUser(1).getLogin());
+   public void testFindUser() {
+        User user = createUser(1, "mail@yandex.ru", "login12", "mail", LocalDate.of(2001, 12, 1));
+        userService.addUser(user);
+        Assertions.assertNotNull(userService.findAllUsers());
     }
 
     @Test
     public void testUpdateUser() {
-        User user = createUser(2, "ivan@yandex.ru", "ivan33", "ivan", LocalDate.of(2008, 10, 4));
+        User user4 = createUser(5, "ira@yandex.ru", "ira23", "ira", LocalDate.of(2006, 7, 7));
+        userService.addUser(user4);
+        User user = createUser(1, "ivan@yandex.ru", "ivan33", "ivan", LocalDate.of(2008, 10, 4));
         userService.updateUser(user);
-        Assertions.assertEquals(user, userService.getUser(2));
+        Assertions.assertEquals(user, userService.getUser(1));
     }
 
     @Test
     public void testFindAllUsers() {
+        User user = createUser(3, "misha@yandex.ru", "misha33", "misha", LocalDate.of(2008, 10, 4));
+        userService.addUser(user);
         Assertions.assertNotNull(userService.findAllUsers());
     }
 
     @Test
     public void testDeleteUser() {
+        User user = createUser(1, "dghfjkg@yandex.ru", "adqqq2", "ddfgrt", LocalDate.of(2001, 12, 1));
+        User user1 = createUser(2, "sdre@yandex.ru", "adswsd", "vwedfg", LocalDate.of(2001, 12, 1));
+        User user2 = createUser(3, "vtter@yandex.ru", "adsere2", "vwdfgrt", LocalDate.of(2001, 12, 1));
+        User user3 = createUser(4, "ujghfj@yandex.ru", "artnyu", "vdfgert", LocalDate.of(2001, 12, 1));
+        User user4 = createUser(5, "sere@yandex.ru", "asertyu2", "vxcvrt", LocalDate.of(2001, 12, 1));
+        userService.addUser(user);
+        userService.addUser(user1);
+        userService.addUser(user2);
+        userService.addUser(user3);
+        userService.addUser(user4);
+
+
         int count = userService.findAllUsers().size();
-        userService.deleteUser(4);
+        userService.deleteUser(5);
         Assertions.assertEquals(count - 1, userService.findAllUsers().size());
     }
 
     @Test
     public void testFriends() {
+        User user = createUser(1, "qwe@yandex.ru", "adsf12", "vwert", LocalDate.of(2001, 12, 1));
+        User user2 = createUser(2, "wer@yandex.ru", "sfdg12", "sdfgr", LocalDate.of(2001, 12, 1));
+        User user3 = createUser(3, "ert@yandex.ru", "dgfh12", "vtware", LocalDate.of(2001, 12, 1));
+        User user4 = createUser(4, "rty@yandex.ru", "fghj12", "tjhgf", LocalDate.of(2001, 12, 1));
+        userService.addUser(user);
+        userService.addUser(user2);
+        userService.addUser(user3);
+        userService.addUser(user4);
         friendService.addFriend(1, 2);
         friendService.addFriend(1, 3);
         Assertions.assertEquals(2, friendService.getFriends(1).size());
@@ -127,7 +141,7 @@ class FilmorateApplicationTests {
         Assertions.assertEquals(6, genreService.getGenres().size());
     }
 
-    private User createUser(int id, String email, String login, String name, LocalDate birthday) {
+    private static User createUser(int id, String email, String login, String name, LocalDate birthday) {
         User user = new User();
         user.setId(id);
         user.setEmail(email);
